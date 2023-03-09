6b6f7e8ebba6501e4c2df065860f9a8845d00c2a
facundoav <125216866+Facundoav@users.noreply.github.com>
2023-03-08 23:48:51 -0300

    Add files via upload
    nuevo proyecto recuperatorio

--- CV/cv.js
+++ CV/cv.js
@@ -1,8 +1,8 @@
 const email = document.getElementById("mail");
 
 email.addEventListener("input", function (event) {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
+  if (com.validity.typeMismatch) {
+    com.setCustomValidity("¡Se esperaba una dirección de correo electrónico!");
   } else {
     email.setCustomValidity("");
   }
